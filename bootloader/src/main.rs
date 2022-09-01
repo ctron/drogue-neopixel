fc6e00c0d588cef1e4e6b154c9b4a0398844bef8
Jens Reimann <jreimann@redhat.com>
2022-09-01 11:45:27 +0200

    chore: update dependencies

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -2,12 +2,8 @@
 #![no_main]
 
 use cortex_m_rt::{entry, exception};
-
-#[cfg(feature = "defmt-rtt")]
+#[cfg(feature = "defmt")]
 use defmt_rtt as _;
-
-use drogue_device::{boards::nrf52::adafruit_feather_nrf52840::*, Board};
-use embassy_boot::FlashConfig;
 use embassy_boot_nrf::*;
 use embassy_nrf::nvmc::Nvmc;
 
@@ -18,76 +14,18 @@ fn main() -> ! {
     // Uncomment this if you are debugging the bootloader with debugger/RTT attached,
     // as it prevents a hard fault when accessing flash 'too early' after boot.
     /*
-    for _i in 0..10000000 {
-        cortex_m::asm::nop();
-    }
+        for i in 0..10000000 {
+            cortex_m::asm::nop();
+        }
     */
 
-    let board = AdafruitFeatherNrf52840::new(p);
     let mut bl = BootLoader::default();
-    let q = board.external_flash.configure();
-    let mut provider = ExampleFlashProvider {
-        nvmc: NvmcFlashConfig {
-            nvmc: WatchdogFlash::start(Nvmc::new(board.nvmc), board.wdt, 5),
-        },
-        qspi: QspiFlashConfig { qspi: q },
-    };
-
-    let start = bl.prepare(&mut provider);
-    core::mem::drop(provider);
-
+    let start = bl.prepare(&mut SingleFlashProvider::new(&mut WatchdogFlash::start(
+        Nvmc::new(p.NVMC),
+        p.WDT,
+        5,
+    )));
     unsafe { bl.load(start) }
-}
-
-pub struct ExampleFlashProvider<'d> {
-    nvmc: NvmcFlashConfig<'d>,
-    qspi: QspiFlashConfig<'d>,
-}
-
-pub struct NvmcFlashConfig<'d> {
-    nvmc: WatchdogFlash<'d>,
-}
-
-impl<'d> FlashConfig for NvmcFlashConfig<'d> {
-    type FLASH = WatchdogFlash<'d>;
-    const ERASE_VALUE: u8 = 0xFF;
-    const BLOCK_SIZE: usize = 4096;
-
-    fn flash(&mut self) -> &mut Self::FLASH {
-        &mut self.nvmc
-    }
-}
-
-pub struct QspiFlashConfig<'d> {
-    qspi: ExternalFlash<'d>,
-}
-
-impl<'d> FlashConfig for QspiFlashConfig<'d> {
-    type FLASH = ExternalFlash<'d>;
-    const ERASE_VALUE: u8 = 0xFF;
-    const BLOCK_SIZE: usize = EXTERNAL_FLASH_BLOCK_SIZE;
-
-    fn flash(&mut self) -> &mut Self::FLASH {
-        &mut self.qspi
-    }
-}
-
-impl<'d> FlashProvider for ExampleFlashProvider<'d> {
-    type STATE = NvmcFlashConfig<'d>;
-    type ACTIVE = NvmcFlashConfig<'d>;
-    type DFU = QspiFlashConfig<'d>;
-
-    fn active(&mut self) -> &mut Self::ACTIVE {
-        &mut self.nvmc
-    }
-
-    fn state(&mut self) -> &mut Self::STATE {
-        &mut self.nvmc
-    }
-
-    fn dfu(&mut self) -> &mut Self::DFU {
-        &mut self.qspi
-    }
 }
 
 #[no_mangle]
